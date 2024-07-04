2d0a69318a5ba263a9f518c449757cc294952449
Donovan <donovan.de.kuiper@ordina.nl>
2024-07-04 20:16:17 +0200

    work in progress

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,38 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="application">
+    <TopAppBar variant="static" color="primary">
+        <Row>
+        <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Kuice</Title>
+        </Section>
+        <Section align="end" toolbar>
+            <IconButton class="material-icons" aria-label="GitHub">github</IconButton>
+            <IconButton class="material-icons" aria-label="Print this page">print</IconButton>
+            <IconButton class="material-icons" aria-label="Bookmark this page">bookmark</IconButton>
+        </Section>
+        </Row>
+    </TopAppBar>
+    <div class="content">
+        Hello World
+    </div>
+</div>
+
+<script lang="ts">
+  import TopAppBar, { Row, Section, Title } from '@smui/top-app-bar';
+  import IconButton from '@smui/icon-button';
+</script>
+
+<style>
+  .application {
+    display: flex;
+    flex-direction: column;
+    height: 100%;
+  }
+
+  .content {
+    flex-basis: 0;
+    height: 0;
+    flex-grow: 1;
+    overflow: auto;
+  }
+</style>
